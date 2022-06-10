6f429f812c3a4a8fdc3afb1d86f33b10619234ec
Ahmet Eren Yılmaz <ahmtylmmz186@gmail.com>
2022-06-10 03:30:41 +0300

    Update index.js

--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/Card'
+import Collapse from './components/Collapse'
 
 function App() {
     return (
         <div>
             <div className="card-group">
-                <Card cardTitle="Samsunspor"/>
-
-                <Card cardTitle="Fenerbahçe"/>
-
-                <Card cardTitle="Bucaspor"/>
+                <Collapse collapseID="collapseExample1">
+                    <Card cardTitle="Samsunspor" />
+                </Collapse>
+                <Collapse collapseID="collapseExample2">
+                    <Card cardTitle="Fenerbahçe" />
+                </Collapse>
+                <Collapse collapseID="collapseExample3">
+                    <Card cardTitle="Bucaspor" />
+                </Collapse>
             </div>
         </div>
     );
