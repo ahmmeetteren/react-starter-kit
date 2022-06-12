b20f2a5f0f641526302cfcc20b8049832380c1b5
Ahmet Eren Yılmaz <ahmtylmmz186@gmail.com>
2022-06-12 10:45:54 +0300

    styling update

--- contact-app/src/components/Contacts/List/index.js
+++ contact-app/src/components/Contacts/List/index.js
@@ -13,15 +13,16 @@ function List({ contacts }) {
   });
   return (
     <div className="w-100">
+      <h1 className="fs-5 mb-3">Contact List</h1>
       <input
         placeholder="Filter contacts"
         value={filterText}
         onChange={(e) => setFilterText(e.target.value)}
         className="form-control mb-2"
       ></input>
-      <ul class="list-group mb-3 mt-3">
+      <ul className="list-group mb-3 mt-3">
         {filtered.map((contact, i) => (
-          <li key={i} class="list-group-item">
+          <li key={i} className="list-group-item">
             <div className="d-inline-block">{contact.fullname}</div>
             <div className="d-inline-block float-end">
               {contact.phonenumber}
