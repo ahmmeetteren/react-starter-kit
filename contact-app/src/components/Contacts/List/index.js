cabb6e6e4ffb5e9ba3b6c8f75d8de752489e80bc
Ahmet Eren Yılmaz <ahmtylmmz186@gmail.com>
2022-06-12 09:51:29 +0300

    Update index.js

--- contact-app/src/components/Contacts/List/index.js
+++ contact-app/src/components/Contacts/List/index.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState } from "react";
 
-function List() {
+function List({ contacts }) {
+  const [filterText, setFilterText] = useState("");
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key]
+        .toString()
+        .toLowerCase()
+        .includes(filterText.toLocaleLowerCase())
+    );
+  });
   return (
-    <div>List</div>
-  )
+    <div>
+      <input
+        placeholder="Filter contacts"
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      ></input>
+      <ul>
+        {filtered.map((contact, i) => (
+          <li key={i}>{contact.fullname}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default List
+export default List;
