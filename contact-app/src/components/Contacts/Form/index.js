27eeca34abc0d52c4819ae2df71d27ef373b68d9
Ahmet Eren Yılmaz <ahmtylmmz186@gmail.com>
2022-06-12 08:31:09 +0300

    added form-components

--- contact-app/src/components/Contacts/Form/index.js
+++ contact-app/src/components/Contacts/Form/index.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
-function Form() {
+const initialFormValues = { fullname: "", phonenumber: "" };
+
+function Form({ setContacts, contacts }) {
+  const [form, setForm] = useState(initialFormValues);
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  useEffect(() => {
+    setForm(initialFormValues);
+  }, [contacts]);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (form.fullname === "" || form.phonenumber === "") {
+      return false;
+    } else {
+      setContacts([...contacts, form]);
+    }
+  };
   return (
-    <div>Form</div>
-  )
+    <form>
+      <div>
+        <input
+          name="fullname"
+          value={form.fullname}
+          placeholder="Full Name"
+          onChange={onChangeInput}
+        ></input>
+      </div>
+      <div>
+        <input
+          name="phonenumber"
+          value={form.phonenumber}
+          placeholder="Phone Number"
+          onChange={onChangeInput}
+        ></input>
+      </div>
+      <button onClick={onSubmit}>Add</button>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
--- contact-app/src/components/Contacts/index.js
+++ contact-app/src/components/Contacts/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import { useState, useEffect } from "react";
 
-import List from './List'
-import Form from './Form'
+import List from "./List";
+import Form from "./Form";
 
-function Contacts(){
-    return (
-        <div>
-            <List/>
-            <Form/>
-        </div>
-    )
+function Contacts() {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
+  return (
+    <div>
+      <List />
+      <Form setContacts={setContacts} contacts={contacts} />
+    </div>
+  );
 }
 
 export default Contacts;
